4a0abbd8e3775f25ef9b37f84a7620c117cc7901
Stefan <stefan.kiers89@hotmail.com>
2025-01-03 12:39:31 +0100

    clearConsole

---
+++ src/main/java/opdracht22/clearConsole.java
@@ -0,0 +1,14 @@
+package opdracht22;
+// clearing the console for the manticore hunting
+// 25 empty lines.
+
+public class clearConsole {
+    public static void main(String[] args) {
+       clearConsole();
+    }
+    public static void clearConsole(){
+        for (int i = 0; i < 25; i++){
+            System.out.println();
+        }
+    }
+}
