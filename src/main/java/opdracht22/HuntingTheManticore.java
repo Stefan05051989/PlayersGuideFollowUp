86dfe412c4aa7eba236c1abb110efda604364cee
Stefan <stefan.kiers89@hotmail.com>
2025-01-07 19:20:34 +0100

    manticore methods

---
+++ src/main/java/opdracht22/HuntingTheManticore.java
@@ -0,0 +1,11 @@
+package opdracht22;
+
+public class HuntingTheManticore {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Hunting The Manticore! ");
+        HuntingTheManticoreGame game = new HuntingTheManticoreGame();
+        Console.clear();
+        System.out.println("Game initialised! ");
+        game.run();
+    }
+}
